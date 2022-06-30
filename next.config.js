c24a6edb1a9ef7c7c9bb5f35ebd61bb607bff7ba
Joe Astuccio <joseph.astuccio@capgemini.com>
2022-06-29 22:17:43 -0400

    Set target to serverless

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const { prepareServerlessUrl } = require('next/dist/server/base-server')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  target: 'serverless',
 }
 
 module.exports = nextConfig
